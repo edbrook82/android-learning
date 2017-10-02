820fe3970005c6a61485d08bd4c0dec2e78db8b8
Ed Brook <e.brook7292@student.leedsbeckett.ac.uk>
2017-10-02 09:40:48 +0100

    Start writing content provider

---
+++ app/src/main/java/uk/co/dekoorb/android/learning/TodoContentProvider.java
@@ -0,0 +1,68 @@
+package uk.co.dekoorb.android.learning;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+
+public class TodoContentProvider extends ContentProvider {
+    private static final int TODOS = 0;
+    private static final int TODOS_ID = 1;
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sUriMatcher.addURI(TodosContract.AUTHORITY, "todos", TODOS);
+        sUriMatcher.addURI(TodosContract.AUTHORITY, "todos/#", TODOS_ID);
+    }
+
+    public TodoContentProvider() {
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // Implement this to handle requests to delete one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        // TODO: Implement this to handle requests for the MIME type of the data
+        // at the given URI.
+        switch (sUriMatcher.match(uri)) {
+            case TODOS:
+                return "vnd.android.cursor.dir/";
+            case TODOS_ID:
+                return "vnd.android.cursor.item";
+            default:
+                throw new UnsupportedOperationException("Unknown Uri");
+        }
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        // TODO: Implement this to handle requests to insert a new row.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public boolean onCreate() {
+        // TODO: Implement this to initialize your content provider on startup.
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        // TODO: Implement this to handle query requests from clients.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        // TODO: Implement this to handle requests to update one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
