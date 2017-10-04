dac2684e3c8d88110f0499c12b27bb8742514b6b
Ed Brook <e.brook7292@student.leedsbeckett.ac.uk>
2017-10-04 08:38:31 +0100

    try out content provider

--- app/src/main/java/uk/co/dekoorb/android/learning/MainActivity.java
+++ app/src/main/java/uk/co/dekoorb/android/learning/MainActivity.java
@@ -1,15 +1,22 @@
 package uk.co.dekoorb.android.learning;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import uk.co.dekoorb.android.learning.model.TodoContract;
 import uk.co.dekoorb.android.learning.view.TodoAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     private TodoAdapter mAdapter;
     private RecyclerView mItemsList;
@@ -31,6 +38,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mAdapter.add();
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(TodoContract.Todo.COLUMN_TITLE, "Test_" + (int) (Math.random()*1e6));
+                contentValues.put(TodoContract.Todo.COLUMN_NOTE, "Just a test note");
+                contentValues.put(TodoContract.Todo.COLUMN_COMPLETED, true);
+
+                Uri uri = getContentResolver().insert(
+                        TodoContract.Todo.CONTENT_URI,
+                        contentValues);
+
+                Log.d(TAG, "onClick: " + uri);
             }
         });
 
@@ -39,6 +56,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mAdapter.sub();
+
+                String[] projection = new String[] {
+                        TodoContract.Todo.COLUMN_TITLE,
+                        TodoContract.Todo.COLUMN_NOTE,
+                        TodoContract.Todo.COLUMN_COMPLETED};
+
+                Cursor cursor = getContentResolver().query(
+                        TodoContract.Todo.CONTENT_URI,
+                        projection,
+                        null,
+                        null,
+                        null);
+
+                if (cursor != null) {
+                    int titleIndex = cursor.getColumnIndex(TodoContract.Todo.COLUMN_TITLE);
+                    int noteIndex = cursor.getColumnIndex(TodoContract.Todo.COLUMN_NOTE);
+                    int completedIndex = cursor.getColumnIndex(TodoContract.Todo.COLUMN_COMPLETED);
+
+                    while (cursor.moveToNext()) {
+                        String title = cursor.getString(titleIndex);
+                        String note = cursor.getString(noteIndex);
+                        boolean completed = cursor.getInt(completedIndex) == 1;
+
+                        Log.d(TAG, String.format("onClick: %s, %s, %s", title, note, completed));
+                    }
+                }
             }
         });
     }
